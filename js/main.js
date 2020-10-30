e94c2aa3297f871437a374b92e554cf5a2245632
KimGyeungMin <kmkm0322@naver.com>
2020-10-30 17:32:07 +0900

    테블릿 슬라이드 OK

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
 $(document).ready(function(){
-    $('.openMOgnb').click(function(){
-        // $(".header_cont").css('display','block');
-        //header아이디영역 백그라운드 추가
-        $(".header_cont").slideDown('slow');
-        $("#header").addClass('on');
-    	//상단으로 바로가기 버튼 클릭
-		$('.to_top a').smoothScroll();
-});
-
-
+	// 상단으로 바로가기 버튼 클릭
+	$(".to_top a").smoothScroll();
+	//alert('콜백함수 실행');
+	$(".openMOgnb").click(function(){
+		//alert();//디버그용
+		//$(".header_cont").css("display","block");
+		$(".header_cont").slideDown("slow");
+		//header 아이디영역 백그라운드 추가
+		$("#header").addClass("on");
+	});
+	$(".closePop").click(function(){
+		//$(".header_cont").css("display","none");
+		$(".header_cont").slideUp("fast");
+		$("#header").removeClass("on");
+	});
 	
-
-$(".closePop").click(function(){
-    // $(".header_cont").css("display","none");
-    $(".header_cont").slideUp('fast');
-    $("#header").removeClass('on');
-
-});
-
 });
